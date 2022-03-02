f9ecf0e95a13f7ef9db03888d5b9d1f09bd4d2e1
agustin-wet <wet.4gustin@gmail.com>
2022-03-02 19:12:42 -0300

    deleted old idea, and just implemented client server communication

---
+++ client.js
@@ -0,0 +1,36 @@
+const net = require('net');
+const readline = require('readline').createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+var socket = new net.Socket();
+var client = socket.connect(8300, 'localhost');
+
+client.setEncoding('utf8');
+
+client.on("connect", () => {
+    socket.write("Hello server, im a new client");
+});
+
+let i = 0;
+
+client.on('data', (data) => {
+
+    if(data === "closeConnection"){
+        console.log("Connection closed");
+        client.destroy();
+    }else{
+        readline.question("Enter a new instruccion : ", (line) => {
+            if(line === "close client"){
+                client.destroy();
+            }
+            socket.write(line);
+        });
+    }
+
+})
+
+client.on('error', (err) => {
+    console.log("Closing client");
+})
--- clientCLI.js
+++
@@ -1,9 +0,0 @@
-const Core = require('./core/core.js');
-
-const action = Core.createAction(process.argv);
-
-if(process.argv.length !== 0){
-    Core.execute(action.command, action.subcommand, action.args);
-}else{
-    console.log("No commands given.\n");
-}
